ab92037923c03c61bfc39915753269316c4a65e6
Eduardo <e.bobato@cinq.com.br>
2019-07-25 18:33:18 -0300

    Inserir novo campo na toolbar

--- app/layout/toolbar/toolbar.controller.js
+++ app/layout/toolbar/toolbar.controller.js
@@ -1,15 +1,14 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('myApp')
-        .controller('ToolbarController', [
-        	'localStorageService','$rootScope', '$scope', '$window', 
-        	function(localStorageService, $rootScope, $scope, $window)
-            {
-        		var self = this;
-        		
-            }
-        ]);
+  angular
+    .module('myApp')
+    .controller('ToolbarController', [
+      'localStorageService', '$rootScope', '$scope', '$window',
+      function (localStorageService, $rootScope, $scope, $window) {
+        var self = this;
+
+      }
+    ]);
 
 })();
